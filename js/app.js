c4fda905a78c7d2272deaff837edf5b98aa19e09
olopcodes <olopcodes@gmail.com>
2024-02-09 14:32:53 -0500

    added data fetching, changed some button styles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,41 @@
-// variables
-const filterButtonEl = $('.world-filter-button');
-const filterRegionListEl = $('.world-region-list');
 
-// show the regoin list
-$(filterButtonEl).click(function (e) { 
-    $(filterRegionListEl).slideToggle();
+$(document).ready(function () {
+    // variables
+    const filterButtonEl = $('.world-filter-button');
+    const filterRegionListEl = $('.world-region-list');
+
+    // show the regoin list
+    $(filterButtonEl).click(function (e) { 
+        $(filterRegionListEl).slideToggle();
+    });
+
+    function getAllCountries () {
+        $.ajax({
+            method: "GET",
+            url: "https://restcountries.com/v3.1/all",
+            success: function (data) {
+                console.log(data)
+            },
+            error: function (err) { 
+                console.log(err)
+             }
+        });
+    }
+
+    function getCountriesByCategory(cat) {
+        $.ajax({
+            method: "GET",
+            url: `https://restcountries.com/v3.1/${cat}/${cat}`,
+            success: function (data) {
+                console.log(data)
+            },
+            error: function (err) { 
+                console.log(err)
+             }
+        });
+    }
+    
+    getCountriesByRegion('europe')
 });
 
 
